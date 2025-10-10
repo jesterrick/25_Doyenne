47757c72d2d689d858a4182e891b4814fe95894b
Rick Jester <rickjester@gmail.com>
2025-10-09 21:06:12 -0400

    Add elevator classes

--- src/main/java/frc/robot/constants/OIConstants.java
+++ src/main/java/frc/robot/constants/OIConstants.java
@@ -2,5 +2,6 @@ package frc.robot.constants;
 
 public class OIConstants {
     public static final int kDriverJoystickPort = 0;
+    public static final int kOperatorJoystickPort = 1;
     public static final double kDriveDeadband = 0.05;
 }
