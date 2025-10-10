17e97cb0e938f01d50378c03cbe208925e4d290a
Rick Jester <rickjester@gmail.com>
2025-10-09 21:42:05 -0400

    setup elevator

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -1,7 +1,16 @@
 package frc.robot.constants;
 
 public class ElevatorConstants {
-    public static final int frontElevMotor = 9;
-    public static final int rearElevMotor = 11;
+    public static final int frontElevMotor = 15;
+    public static final int rearElevMotor = 16;
     public static final int encoderElev = 8;
+
+    public static final double kElevatorChainRatio = 10;
+    public static final int kMaxElevatorHeight = 0;
+
+    public static final int kElevatorEncoderDIOPort1 =  3;
+    public static final int kElevatorEncoderDIOPort2 =  2;
+    
+    public static final int kLowerLimitSwitchDIOPort = 0;
+    public static final int kUpperLimitSwitchDIOPort = 1;
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,15 +4,51 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ElevatorConstants;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class Elevator extends SubsystemBase {
-  /** Creates a new Elevator. */
-  public Elevator() {}
+
+  Encoder elevatorEncoder;
+  DigitalInput lowerLimitSwitch;
+  DigitalInput upperLimitSwitch;
+  SparkMax elevatorDriveMotor1;
+  SparkMax elevatorDriveMotor2;
+
+  public Elevator() {
+    this.elevatorEncoder = new Encoder(ElevatorConstants.encoderElev, ElevatorConstants.encoderElev + 1);
+    this.lowerLimitSwitch = new DigitalInput(ElevatorConstants.kLowerLimitSwitchDIOPort);  
+    this.upperLimitSwitch = new DigitalInput(ElevatorConstants.kUpperLimitSwitchDIOPort);
+    this.elevatorDriveMotor1 = new SparkMax(ElevatorConstants.frontElevMotor, MotorType.kBrushless);
+    this.elevatorDriveMotor2 = new SparkMax(ElevatorConstants.rearElevMotor, MotorType.kBrushless);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public boolean isUpperLimitSwitchActive() {
+    return !this.upperLimitSwitch.get(); // Inverted logic
+  }
+
+  public boolean isLowerLimitSwitchActive() {
+    return !this.lowerLimitSwitch.get();
+  }
+
+  public double getElevatorPosition() {
+    this.elevatorEncoder.setDistancePerPulse(.01);
+    if (isLowerLimitSwitchActive()) {
+      this.elevatorEncoder.reset();
+        System.out.println("Lower limit switch triggered. Encoder reset.");
+    }
+    double position = this.elevatorEncoder.getDistance();
+    System.out.println("Current Encoder Position: " + position);
+    return position;
   }
 }
