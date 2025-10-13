21a4975a5475e2c8bf8936b9580c5afa1ab24c13
RJester17 <rachel.n.jester@gmail.com>
2025-10-12 21:33:35 -0400

    Define Buttons

--- src/main/java/frc/robot/constants/OIConstants.java
+++ src/main/java/frc/robot/constants/OIConstants.java
@@ -4,4 +4,14 @@ public class OIConstants {
     public static final int kDriverJoystickPort = 0;
     public static final int kOperatorJoystickPort = 1;
     public static final double kDriveDeadband = 0.05;
+
+    public static final int kElevatorPositionButton0 = 0;
+    public static final int kElevatorPositionButton1 = 1;
+    public static final int kElevatorPositionButton2 = 2;
+    public static final int kElevatorPositionButton3 = 3;
+    public static final int kElevatorPositionButton4 = 4;
+
+    public static final int kOuttakeEjectButton = 6;
+    public static final int kIntakeToOuttakeButton = 7;
+    public static final int kIntakeReveiveButton = 5;
 }
