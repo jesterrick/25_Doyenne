ce33f5736277861d28f7c45e86ce392017685f5b
RJester17 <rachel.n.jester@gmail.com>
2025-10-12 20:41:43 -0400

    Created Intake Commands

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,5 +1,11 @@
 package frc.robot.constants;
 
 public class IntakeConstants {
+    public static final int kIntakeMotor1 = 20;
+    public static final int kIntakeMotor2 = 21;
+    public static final int kIntakeMotor3 = 22;
+
+    public static final double kIntakeMotorSpeed = 0.2;
+
 
 }
