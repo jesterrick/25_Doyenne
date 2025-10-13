95d033c97655a17a932c20a3ddd5146dbc96363a
RJester17 <rachel.n.jester@gmail.com>
2025-10-12 20:43:02 -0400

    Name Change

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.constants;
 
 public class IntakeConstants {
-    public static final int kIntakeMotor1 = 20;
-    public static final int kIntakeMotor2 = 21;
-    public static final int kIntakeMotor3 = 22;
+    public static final int kIntakeMotorFront = 20;
+    public static final int kIntakeMotorBack = 21;
+    public static final int kIntakeMotorCenter = 22;
 
     public static final double kIntakeMotorSpeed = 0.2;
 
