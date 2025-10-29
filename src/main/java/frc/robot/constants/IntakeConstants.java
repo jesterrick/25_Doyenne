cc8227d38f37654976df937df25c8e52eb590907
RJester17 <rachel.n.jester@gmail.com>
2025-10-29 19:39:51 -0400

    Intake Reload time

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -7,6 +7,6 @@ public class IntakeConstants {
     public static final double kIntakeEjectSpeed = 0.8;
     public static final double kIntakeEjectDuration = 0.5;
     public static final double kIntakeCenterDuration = 0.4;
-    public static final double kIntakeReloadTime = 1.0;
+    public static final double kIntakeReloadTime = 0.5;
 
 }
--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -3,7 +3,8 @@ package frc.robot.constants;
 public class VisionConstants {
 public static final int TEAM_NUMBER = 5919;
 public static final String LIMELIGHT_NAME = "limelight";
-public static final String LIMELIGHT_URL = "http://10.59.19.11:5800";
+//public static final String LIMELIGHT_URL = "http://10.59.19.11:5800";
+public static final String LIMELIGHT_URL = "http://limelight.local:5800";
 
 public static final int DRIVER_PIPELINE = 0;
 public static final int VISION_PIPELINE = 1;
